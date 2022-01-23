886fa41aebc38f658e3640964005765defd6078c
ellandor <66072698+ellandor@users.noreply.github.com>
2022-01-23 09:52:23 -0300

    update main page

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -19,55 +19,61 @@ export default function App() {
             </Head>
 
             <div className='min-h-screen font-montserrat'>
-                <div className='min-w-screen flex justify-center bg-gray-900 py-4'>
+                <div className='min-w-screen flex justify-center bg-black-light py-4'>
                     <span className='text-white font-medium'>Ganhe R$ 10,00 de desconto no frete</span>
                 </div>
 
-                <header className='min-w-screen flex justify-center py-4'>
+                <header className='min-w-screen flex justify-center py-3'>
                     <Link href='/' passHref>
-                        <a className='font-semibold text-lg'>JordanShoes</a>
+                        <a className='font-semibold text-2xl'>
+                            <h1>JordanShoes</h1>
+                        </a>
                     </Link>
                 </header>
 
                 <main className='min-w-screen'>
-                    <section className='relative h-[400px] flex items-center before:bg-[url("/main-image-mobile.webp")] before:bg-cover before:bg-no-repeat before:w-full before:h-full before:absolute before:brightness-50 before:-z-10 md:before:bg-[url("/main-image-desktop.webp")]'>
-                        <div className='text-white px-6 pl-[10%]'>
-                            <h2 className='text-3xl font-semibold mb-4'>A melhor loja de Jordan</h2>
-                            <h3 className='font-medium leading-[1.8] max-w-[410px]'>
+                    <section className='relative h-[400px] flex items-center justify-center md:justify-start before:bg-[url("/main-image-mobile.webp")] before:bg-cover before:bg-no-repeat before:w-full before:h-full before:absolute before:brightness-[0.4] before:-z-10 md:before:bg-[url("/main-image-desktop.webp")]'>
+                        <div className='text-white px-6 md:pl-[20%]'>
+                            <h2 className='text-4xl font-semibold mb-4'>A melhor loja de Jordan</h2>
+                            <h3 className='font-medium leading-[1.8] max-w-[450px] text-lg'>
                                 O tênis Jordan é fruto de uma velha e forte parceria entre a Nike e o jogador
                                 Michael Jordan.
                             </h3>
                         </div>
                     </section>
 
-                    <section className='flex flex-col items-center px-6 py-20 gap-y-4'>
-                        <h2 className='font-semibold text-3xl'>Destaques</h2>
-                        <h3 className='font-medium text-center leading-[1.8] text-gray-700'>
-                            Frete grátis e chinelo de brinde é aqui, aproveite por tempo limitado.
-                        </h3>
-                    </section>
+                    <div className='bg-_white-100'>
+                        <section className='flex flex-col items-center px-6 py-20 gap-y-4'>
+                            <h2 className='font-semibold text-4xl'>Destaques</h2>
+                            <h3 className='font-medium text-center leading-[1.8] text-gray-700 text-lg'>
+                                Frete grátis e chinelo de brinde é aqui, aproveite por tempo limitado.
+                            </h3>
+                        </section>
 
-                    <section>
-                        <nav className='pb-12 flex justify-center'>
-                            <ul className='px-4 grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 justify-items-center gap-x-2 gap-y-6 container'>
+                        <section className='flex justify-center pb-20'>
+                            <div className='px-4 grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 justify-items-center gap-x-2 gap-y-6'>
                                 {images.map((image, i) => (
-                                    <li key={image} className='relative w-full max-w-[380px] h-[300px]'>
+                                    <div
+                                        key={image}
+                                        className='relative w-full max-w-[350px] bg-_white-200 px-8 py-12 flex items-center justify-center'
+                                    >
                                         <Image
                                             src={image}
-                                            layout='fill'
                                             alt={`Image ${i + 1}`}
                                             quality={100}
+                                            width={250}
+                                            height={150}
                                             loading='lazy'
                                         />
-                                    </li>
+                                    </div>
                                 ))}
-                            </ul>
-                        </nav>
-                    </section>
+                            </div>
+                        </section>
+                    </div>
                 </main>
 
-                <footer className='min-w-screen flex justify-center bg-gray-900 py-4'>
-                    <span className='text-white font-medium'>Todos os direitos reservados</span>
+                <footer className='min-w-screen flex justify-center bg-black-light py-4'>
+                    <span className='text-white'>Todos os direitos reservados</span>
                 </footer>
             </div>
         </>
